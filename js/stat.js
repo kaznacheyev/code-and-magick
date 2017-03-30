f2696f58066f3c0147f546076ca8d75e545f9ba6
Yaroslav Kaznacheyev <kaznacheyev88@gmail.com>
2017-03-30 10:12:53 +0300

    Corrections eslint

--- js/stat.js
+++ js/stat.js
@@ -14,34 +14,32 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Список результатов:', 120, 60);
 
   var max = -1;
-  var maxIndex = -1;
 
   for (var i = 0; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
-      maxIndex = i;
     }
   }
 
   var histogramHeight = 150;
   var step = histogramHeight / max;
-
   var barWidth = 40;
   var indent = 50;
   var initialX = 140;
   var initialY = 90;
+  var nameOffsetY = 20;
+  var timeOffsetY = 10;
 
-  for (var i = 0; i < times.length; i++) {
+  for (i = 0; i < times.length; i++) {
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = 'rgba(0, 0, 255, ' + (Math.random() * (1.0 - 0.3) + 0.3) + ')';
     }
     ctx.fillRect(initialX + (barWidth + indent) * i, initialY + histogramHeight - times[i] * step, barWidth, times[i] * step);
-
     ctx.fillStyle = '#000000';
-    ctx.fillText(times[i].toFixed(0), initialX + (barWidth + indent) * i, initialY + histogramHeight - times[i] * step - 10);
-    ctx.fillText(names[i], initialX + (barWidth + indent) * i, initialY + histogramHeight + 20);
+    ctx.fillText(times[i].toFixed(0), initialX + (barWidth + indent) * i, initialY + histogramHeight - times[i] * step - timeOffsetY);
+    ctx.fillText(names[i], initialX + (barWidth + indent) * i, initialY + histogramHeight + nameOffsetY);
   }
-}
+};
